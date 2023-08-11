d20bc3ac07e9102945dda7498c0fc0072d0cc58e
HafizSp <hafizsp45@gmail.com>
2023-08-11 13:36:54 +0600

    updated

--- lib/module2_assignment.dart
+++ lib/module2_assignment.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<int> grades = [85, 92, 78, 65, 88, 72];
+  List<int> grades = [85, 92, 78, 65, 88, 7];
   int sum = 0;
   for (int i = 0; i < grades.length; i++) {
     sum += grades[i];
@@ -7,10 +7,10 @@ void main() {
 
   double average = sum / grades.length;
   print("Student's average grade: ${average.toStringAsFixed(1)}");
-  int round = average.toInt();
-  print("Rounded average: $round");
+  int roundAverage = average.round();
+  print("Rounded average: $roundAverage");
 
-  if (round >= 70) {
+  if (roundAverage >= 70) {
     print('Passed');
   } else {
     print('Failed');
